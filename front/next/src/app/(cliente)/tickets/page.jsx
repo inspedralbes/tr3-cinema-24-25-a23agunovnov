ac27a002c8a7950ed9233ae35103d29eb1fd99b2
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-03-11 22:18:53 +0100

    prova final

--- front/next/src/app/(cliente)/tickets/page.jsx
+++ front/next/src/app/(cliente)/tickets/page.jsx
@@ -9,7 +9,7 @@ export default function Tickets() {
             <div className="max-w-7xl mx-auto px-4">
                 <h1 className="text-3xl font-bold mb-8">Mis Tickets</h1>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     {mockTickets.map((ticket) => (
                         <div
                             key={ticket.id}
@@ -62,7 +62,7 @@ export default function Tickets() {
                             </div>
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
     </>
