3418b32135ab565b7562fea080c3f3067ca32054
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-03-24 17:59:07 +0100

    Creació de login al controlador

--- back/laravel/app/Http/Controllers/AdminController.php
+++ back/laravel/app/Http/Controllers/AdminController.php
@@ -9,49 +9,6 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|unique:admins,email',
-            'password' => 'required|string',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'error' => $validator->errors()
-            ], 422);
-        }
-
-        $admin = Admin::create([
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
-
-        return response()->json([
-            'message' => 'ADMIN creado exitosamente.',
-            'admin' => $admin
-        ], 201);
-    }
-
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -78,41 +35,10 @@ class AdminController extends Controller
         $token = $admin->createToken('API Token')->plainTextToken;
 
         return response()->json([
+            'success' => true,
             'message' => 'Inicio de sesión exitoso.',
             'user' => $admin,
             'token' => $token
         ], 200);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Admin $admin)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Admin $admin)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Admin $admin)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Admin $admin)
-    {
-        //
     }
 }
